fa192b64ebd6609333cf59cfae690580140066d5
jiawun <ss10232301995@gmail.com>
2024-02-04 09:40:33 +0800

    feat: add livePro query

--- FarmerPro/Controllers/ProductController.cs
+++ FarmerPro/Controllers/ProductController.cs
@@ -10,6 +10,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Results;
 
 namespace FarmerPro.Controllers
 {
@@ -91,8 +92,103 @@ namespace FarmerPro.Controllers
                 return Content(HttpStatusCode.OK, result);
             }
         }
+        #endregion
+
+        #region FGP-2 取得live產品、熱銷產品、特價促銷產品、水果產品、蔬菜產品
+        [HttpGet]
+        //自定義路由
+        [Route("api/product/liveProduct")]
+        //使用 IHttpActionResult 作為返回 HTTP 回應類型
+
+        public IHttpActionResult productindex()
+        {
+            try
+            {
+                var dayOfWeek = new Dictionary<DayOfWeek, string>
+                {
+                    { DayOfWeek.Sunday, "日" },
+                    { DayOfWeek.Monday, "一" },
+                    { DayOfWeek.Tuesday, "二" },
+                    { DayOfWeek.Wednesday, "三" },
+                    { DayOfWeek.Thursday, "四" },
+                    { DayOfWeek.Friday, "五" },
+                    { DayOfWeek.Saturday, "六" }
+                };
+
+                var liveProduct = from p in db.Products
+                                  join user in db.Users on p.UserId equals user.Id
+                                  join s in db.Specs on p.Id equals s.ProductId
+                                  from livepro in db.LiveProducts.Where(lp => p.Id == lp.SpecId).DefaultIfEmpty()
+                                  from liveset in db.LiveSettings.Where(ls => user.Id == ls.UserId).DefaultIfEmpty()
+                                  from album in db.Albums.Where(a => p.Id == a.ProductId).DefaultIfEmpty()
+                                  let photo = db.Photos.FirstOrDefault(ph => album != null && album.Id == ph.AlbumId)
+                                  where p.ProductState && s != null && s.LivePrice.HasValue && s.LivePrice.Value != 0
+                                  orderby p.CreatTime descending
+                                  select new
+                                  {
+                                      productId = p.Id,
+                                      productTitle = p.ProductTitle,
+                                      livePrice = s.LivePrice,
+                                      farmerName = user.NickName,
+                                      //liveDate = liveset != null ? liveset.LiveDate.ToString("yyyy.MM.dd") + dayOfWeek[liveset.LiveDate.DayOfWeek] + liveset.StartTime.ToString("HH:mm") : "",
+                                      productImg = new
+                                      {
+                                          src = photo != null ? photo.URL : "default-src",
+                                          alt = p.ProductTitle
+                                      },
+                                      farmerImg = new
+                                      {
+                                          src = user.Photo != null ? user.Photo : "default-src",
+                                          alt = user.NickName
+                                      }
+                                  };
+
+
+
+               
+
+                if (!liveProduct.Any())
+                {
+                    //result訊息
+                    var result = new
+                    {
+                        statusCode = 400,
+                        status = "error",
+                        message = "取得失敗",
+                    };
+                    return Content(HttpStatusCode.OK, result);
+                }
+                else
+                {
+
+                    // result 訊息
+                    var result = new
+                    {
+                        statusCode = 200,
+                        status = "success",
+                        message = "取得成功",
+                        data = liveProduct.ToList()
+                    };
+                    return Content(HttpStatusCode.OK, result);
+                }
+            }
+            catch
+            {
+                //result訊息
+                var result = new
+                {
+                    statusCode = 500,
+                    status = "error",
+                    message = "其他錯誤",
+                };
+                return Content(HttpStatusCode.OK, result);
+            }
+        }
+        #endregion
     }
-    #endregion
+
+
+
 
     public class GetAllProduct
     {
@@ -119,6 +215,36 @@ namespace FarmerPro.Controllers
 
         [Display(Name = "相片alt")]
         public string alt { get; set; }
+    }
+
+    public class GetLiveProduct
+    {
+        [Display(Name = "商品編號")]
+        public int productId { get; set; }
+
+        [Display(Name = "農產品名稱")]
+        public string productTitle { get; set; }
+
+        [Display(Name = "直播價")]
+        public int livePrice { get; set; }
+
+        [Display(Name = "小農姓名")]
+        public int farmerName { get; set; }
+
+        [Display(Name = "直播日期")]
+        public int liveDate { get; set; }
+
+        [Display(Name = "相片路徑物件")]
+        public string productImg { get; set; }
+
+        [Display(Name = "相片路徑")]
+        public string src { get; set; }
+
+        [Display(Name = "相片alt")]
+        public string alt { get; set; }
+
+        [Display(Name = "小農照片物件")]
+        public string farmerImg { get; set; }
     }
 }
 
