d263e2d524534f919674973d9730a48d8d6c7e4f
jiawun <ss10232301995@gmail.com>
2024-02-04 10:56:11 +0800

    feat: add promotionPro query

--- FarmerPro/Controllers/ProductController.cs
+++ FarmerPro/Controllers/ProductController.cs
@@ -172,9 +172,42 @@ namespace FarmerPro.Controllers
                                          },
                                      };
 
+                var promotionProduct = from p in db.Products
+                                       join user in db.Users on p.UserId equals user.Id
+                                       join s in db.Specs on p.Id equals s.ProductId
+                                       from album in db.Albums.Where(a => p.Id == a.ProductId).DefaultIfEmpty()
+                                       let photo = db.Photos.FirstOrDefault(ph => album != null && album.Id == ph.AlbumId)
+                                       where p.ProductState
+                                       orderby p.CreatTime descending
+                                       select new
+                                       {
+                                           productId = p.Id,
+                                           productTitle = p.ProductTitle,
+                                           farmerName = user.NickName,
+                                           origin = p.Origin,
+                                           smallOriginalPrice = s.Price,
+                                           smallPromotionPrice = s.PromotePrice,
+                                           productImg = new
+                                           {
+                                               src = photo != null ? photo.URL : "default-src",
+                                               alt = p.ProductTitle
+                                           },
+                                           farmerImg = new
+                                           {
+                                               src = user.Photo != null ? user.Photo : "default-src",
+                                               alt = user.NickName
+                                           }
+                                       };
+
+                // 执行查询并将结果转换为列表
+                var promotionProducts = promotionProduct.ToList();
+
+                // 在内存中随机排序并取前四条记录
+                var randomPromotionProducts = promotionProducts.OrderBy(x => Guid.NewGuid()).Take(4).ToList();
 
 
-                if (!liveProduct.Any())
+
+                if (!promotionProduct.Any())
                 {
                     //result訊息
                     var result = new
@@ -194,7 +227,7 @@ namespace FarmerPro.Controllers
                         statusCode = 200,
                         status = "success",
                         message = "取得成功",
-                        data = liveProduct.ToList()
+                        data = promotionProduct.ToList()
                     };
                     return Content(HttpStatusCode.OK, result);
                 }
