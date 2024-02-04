544e34ef99a0a841d14df05b94f00943beb69fa7
jiawun <ss10232301995@gmail.com>
2024-02-04 15:16:06 +0800

    feat:FGP03-API add detailPro query

--- FarmerPro/Controllers/ProductController.cs
+++ FarmerPro/Controllers/ProductController.cs
@@ -97,7 +97,7 @@ namespace FarmerPro.Controllers
         #region FGP-2 取得live產品、熱銷產品、特價促銷產品、水果產品、蔬菜產品
         [HttpGet]
         //自定義路由
-        [Route("api/product/liveProduct")]
+        [Route("api/product/productIndex")]
         ///api/product?(liveqty=3)&(topsalesqty)&(promoteqty=6)&(fruitqyt)&(vegatqty)
         //使用 IHttpActionResult 作為返回 HTTP 回應類型
 
@@ -300,10 +300,126 @@ namespace FarmerPro.Controllers
             }
         }
         #endregion
+
+        #region FGP-3 取得特定商品細節資訊(包含小農介紹、小農產品推薦4筆)
+        [HttpGet]
+        //自定義路由
+        [Route("api/product/{productId}")]
+        //使用 IHttpActionResult 作為返回 HTTP 回應類型
+        //判斷大小價錢
+        public IHttpActionResult productdetail(int productId)
+        {
+            try
+            {
+                var detailProduct = from p in db.Products
+                                  join user in db.Users on p.UserId equals user.Id
+                                  join s in db.Specs on p.Id equals s.ProductId
+                                  from album in db.Albums.Where(a => p.Id == a.ProductId).DefaultIfEmpty()
+                                  where p.ProductState && p.Id == productId
+                                  orderby p.CreatTime descending
+                                  let largeSpec = db.Specs.FirstOrDefault(s => s.ProductId == p.Id && !s.Size) //大= F
+                                  let smallSpec = db.Specs.FirstOrDefault(s => s.ProductId == p.Id && s.Size)  //小 = T
+
+                                  select new
+                                  {
+                                      productId = p.Id,
+                                      productTitle = p.ProductTitle,
+                                      category = p.Category,
+                                      period = p.Period.ToString(),
+                                      origin = p.Origin.ToString(),
+                                      storage = p.Storage.ToString(),
+                                      productDescription = p.Description,
+                                      introduction = p.Introduction,
+                                      largeOriginalPrice = largeSpec != null ? (int?)largeSpec.Price : null,
+                                      largePromotionPrice = largeSpec != null ? (int?)largeSpec.PromotePrice : null,
+                                      largeWeight = largeSpec != null ? (int?)largeSpec.Weight : null,
+                                      largeStock = largeSpec != null ? (int?)largeSpec.Stock : null,
+                                      smallOriginalPrice = smallSpec != null ? (int?)smallSpec.Price : null,
+                                      smallPromotionPrice = smallSpec != null ? (int?)smallSpec.PromotePrice : null,
+                                      smallWeight = smallSpec != null ? (int?)smallSpec.Weight : null,
+                                      smallStock = smallSpec != null ? (int?)smallSpec.Stock : null,
+                                      productImages = db.Photos.Where(ph => album != null && album.Id == ph.AlbumId).Select(ph => new
+                                      {
+                                          src = ph.URL,
+                                          alt = p.ProductTitle
+                                      }).ToList(),
+                                      farmerName = user.NickName,
+                                      farmervision = user.Vision,
+                                      farmerdescription = user.Description,
+                                      farmerImg = new
+                                      {
+                                          src = user.Photo != null ? user.Photo : "default-src",
+                                          alt = user.NickName
+                                      }
+                                  };
+
+                //取得Product、Spec、Album、Photo的聯合資料
+                var productInfoByUser = from p in db.Products
+                                  join s in db.Specs on p.Id equals s.ProductId
+                                  from album in db.Albums.Where(a => p.Id == a.ProductId).DefaultIfEmpty()
+                                  let photo = db.Photos.FirstOrDefault(ph => album != null && album.Id == ph.AlbumId)
+                                  where p.ProductState && !s.Size // 確認p.ProductState = true && s.Size = false
+                                  orderby p.CreatTime descending
+                                  select new
+                                  {
+                                      productId = p.Id,
+                                      productTitle = p.ProductTitle,
+                                      smallOriginalPrice = s.Price,
+                                      smallPromotionPrice = s.PromotePrice,
+                                      productImg = new
+                                      {
+                                          src = photo != null ? photo.URL : "default-src",
+                                          alt = p.ProductTitle
+                                      }
+
+                                  };
+
+                if (!detailProduct.Any())
+                {
+                    //result訊息
+                    var result = new
+                    {
+                        statusCode = 400,
+                        status = "error",
+                        message = "取得失敗",
+                    };
+                    return Content(HttpStatusCode.OK, result);
+                }
+                else
+                {
+
+                    // result 訊息
+                    var result = new
+                    {
+                        statusCode = 200,
+                        status = "success",
+                        message = "取得成功",
+                        //data = new
+                        //{
+                        //    detailProduct = detailProduct.ToList(),
+                        //    productInfoByUser = productInfoByUser.ToList(),
+                        //}
+                        data = detailProduct.ToList()
+                    };
+                    return Content(HttpStatusCode.OK, result);
+                }
+            }
+            catch
+            {
+                //result訊息
+                var result = new
+                {
+                    statusCode = 500,
+                    status = "error",
+                    message = "其他錯誤",
+                };
+                return Content(HttpStatusCode.OK, result);
+            }
+        }
+        #endregion
+
+
     }
-
-
-
 
     public class GetAllProduct
     {
