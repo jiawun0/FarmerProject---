637f5d6d130f00c836368cbf3835aa7871e952b9
jiawun <ss10232301995@gmail.com>
2024-02-06 16:47:05 +0800

    feat: add FGC01-API

--- FarmerPro/Controllers/CartController.cs
+++ FarmerPro/Controllers/CartController.cs
@@ -1,7 +1,10 @@
 ﻿using FarmerPro.Models;
+using FarmerPro.Models.ViewModel;
 using FarmerPro.Securities;
+using Microsoft.Ajax.Utilities;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
@@ -19,15 +22,15 @@ namespace FarmerPro.Controllers
         //自定義路由
         [Route("api/cart")]
         //[JwtAuthFilter]
-        
+
         //使用 IHttpActionResult 作為返回 HTTP 回應類型
         public IHttpActionResult GetCartItem()
         {
             try
             {
                 int CustomerId = Convert.ToInt16(JwtAuthFilter.GetToken(Request.Headers.Authorization.Parameter)["Id"]);
-                
-                var cartInfo = db.Carts.Where(c => c.UserId == CustomerId).Include(c=> c.CartItem)
+
+                var cartInfo = db.Carts.Where(c => c.UserId == CustomerId).Include(c => c.CartItem)
                                   .Select(cart => new
                                   {
                                       totalOriginalPrice = cart.CartItem.Sum(c => c.Spec.Price * c.Qty),
@@ -39,25 +42,25 @@ namespace FarmerPro.Controllers
                                       totalDiscount = d.totalOriginalPrice - d.totalPromotionPrice,
                                   });
 
-                var cartItemInfo = db.Carts.Where(c => c.UserId == CustomerId).SelectMany(c=>c.CartItem)
+                var cartItemInfo = db.Carts.Where(c => c.UserId == CustomerId).SelectMany(c => c.CartItem)
                                            .Select(cartItem => new
+                                           {
+                                               productId = cartItem.Spec.ProductId,
+                                               productTitle = cartItem.Spec.Product.ProductTitle, // Spec--Product--Title
+                                               productSpecId = cartItem.Spec.Id,
+                                               productSpecSize = cartItem.Spec.Size,
+                                               productSpecWeight = (int)cartItem.Spec.Weight,
+                                               cartItemOriginalPrice = cartItem.Spec.Price,
+                                               cartItemPromotionPrice = cartItem.Spec.PromotePrice,
+                                               cartItemQty = cartItem.Qty,
+                                               subtotal = cartItem.Qty * cartItem.Spec.PromotePrice,
+                                               //ProductImg = cartItem.Spec.Product.,photo != null ? photo.URL : "default-src",
+                                               productImg = new
                                                {
-                                                   productId = cartItem.Spec.ProductId,
-                                                   productTitle = cartItem.Spec.Product.ProductTitle, // Spec--Product--Title
-                                                   productSpecId = cartItem.Spec.Id,
-                                                   productSpecSize = cartItem.Spec.Size,
-                                                   productSpecWeight = (int)cartItem.Spec.Weight,
-                                                   cartItemOriginalPrice = cartItem.Spec.Price,
-                                                   cartItemPromotionPrice = cartItem.Spec.PromotePrice,
-                                                   cartItemQty = cartItem.Qty,
-                                                   subtotal = cartItem.Qty * cartItem.Spec.PromotePrice,
-                                                   //ProductImg = cartItem.Spec.Product.,photo != null ? photo.URL : "default-src",
-                                                   productImg = new
-                                                   {
-                                                       src = db.Albums.Where(a => a.ProductId == cartItem.Spec.ProductId).FirstOrDefault().Photo.FirstOrDefault().URL ?? "default-src",
-                                                       alt = cartItem.Spec.Product.ProductTitle,
-                                                   },
-                                               }).ToList();
+                                                   src = db.Albums.Where(a => a.ProductId == cartItem.Spec.ProductId).FirstOrDefault().Photo.FirstOrDefault().URL ?? "default-src",
+                                                   alt = cartItem.Spec.Product.ProductTitle,
+                                               },
+                                           }).ToList();
 
                 var cartItemLength = cartItemInfo.Count();
 
@@ -106,6 +109,146 @@ namespace FarmerPro.Controllers
         }
         #endregion
 
-        
+        #region FGC-1 加入購物車(要補上購物車數量欄位)
+        [HttpPost]
+        //自定義路由
+        [Route("api/cart")]
+        //[JwtAuthFilter]
+
+        //使用 IHttpActionResult 作為返回 HTTP 回應類型
+        public IHttpActionResult AddCartItem([FromBody] GetCartItemClass input)
+        {
+            try
+            {
+                int CustomerId = Convert.ToInt16(JwtAuthFilter.GetToken(Request.Headers.Authorization.Parameter)["Id"]);
+
+                //如果沒有就創建
+                var cart = db.Carts.FirstOrDefault(c => c.UserId == CustomerId);
+                if (cart == null)
+                {
+                    cart = new Cart { UserId = CustomerId };
+                    db.Carts.Add(cart);
+                    db.SaveChanges();
+                }
+
+                var CartId = cart.Id;
+                var SpecId = input.productSpecId; //沒有此商品，請重新輸入
+                var Qty = input.cartItemQty;  //數量不可為0
+                var SubTotal = (int)input.subtotal;
+
+                var SpecInfo = db.Specs.FirstOrDefault(s => s.Id == SpecId);
+
+                if (SpecInfo == null)
+                {
+                    //result訊息
+                    var result = new
+                    {
+                        statusCode = 401,
+                        status = "error",
+                        message = "沒有商品SpecId，請重新輸入",
+                    };
+                    return Content(HttpStatusCode.OK, result);
+                }
+                else if (Qty <= 0)
+                {
+                    //result訊息
+                    var result = new
+                    {
+                        statusCode = 402,
+                        status = "error",
+                        message = "數量不可為0，請重新輸入",
+                    };
+                    return Content(HttpStatusCode.OK, result);
+                }
+                else
+                {
+
+                    //加入商品
+                    var addcartitem = new CartItem
+                    {
+                        CartId = cart.Id,
+                        SpecId = input.productSpecId, //沒有此商品，請重新輸入
+                        Qty = input.cartItemQty,  //數量不可為0
+                        SubTotal = (int)input.subtotal,
+                    };
+                    db.CartItems.Add(addcartitem);
+                    db.SaveChanges();
+
+                    var cartItemInfo = db.CartItems.Where(c => c.Cart.UserId == CustomerId)
+                                               .Select(cartItem => new
+                                               {
+                                                   productId = cartItem.Spec.ProductId,
+                                                   productTitle = cartItem.Spec.Product.ProductTitle, // Spec--Product--Title
+                                                   productSpecId = cartItem.Spec.Id,
+                                                   productSpecSize = cartItem.Spec.Size,
+                                                   productSpecWeight = (int)cartItem.Spec.Weight,
+                                                   cartItemOriginalPrice = cartItem.Spec.Price,
+                                                   cartItemPromotionPrice = cartItem.Spec.PromotePrice,
+                                                   cartItemQty = cartItem.Qty,
+                                                   subtotal = cartItem.Qty * cartItem.Spec.PromotePrice,
+                                                   productImg = new
+                                                   {
+                                                       src = db.Albums.Where(a => a.ProductId == cartItem.Spec.ProductId).FirstOrDefault().Photo.FirstOrDefault().URL ?? "default-src",
+                                                       alt = cartItem.Spec.Product.ProductTitle,
+                                                   },
+                                               }).ToList();
+
+
+                    if (!cartItemInfo.Any())
+                    {
+                        //result訊息
+                        var result = new
+                        {
+                            statusCode = 200,
+                            status = "success",
+                            message = "沒有結果",
+                            data = new object[] { }
+                        };
+                        return Content(HttpStatusCode.OK, result);
+                    }
+                    else
+                    {
+
+                        // result 訊息
+                        var result = new
+                        {
+                            statusCode = 200,
+                            status = "success",
+                            message = "新增成功",
+                            data = cartItemInfo
+                        };
+                        return Content(HttpStatusCode.OK, result);
+                    }
+                }
+            }
+            catch
+            {
+                //result訊息
+                var result = new
+                {
+                    statusCode = 500,
+                    status = "error",
+                    message = "其他錯誤",
+                };
+                return Content(HttpStatusCode.OK, result);
+            }
+        }
+        #endregion
+
+
+        public class GetCartItemClass
+        {
+            [Display(Name = "農產品編號")]
+            public int productId { get; set; }
+
+            [Display(Name = "規格spec編號")]
+            public int productSpecId { get; set; }
+
+            [Display(Name = "數量")]
+            public int cartItemQty { get; set; }
+
+            [Display(Name = "小計")]
+            public double subtotal { get; set; }
+        }
     }
 }
